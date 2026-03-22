84211629ff659027d7fdc63a03903efb73d9f2a1
Malvin-26 <malvinashoksamuel@gamil.com>
2026-03-22 12:11:20 +0530

    fix local

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+window.SUPABASE_URL = 'https://noycbedgikhggbkxwbxv.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_RJSsRiMEa-5aP1KyKkrH_A_KMChguEp';
+window.SUPABASE_USERS_TABLE = 'users';
